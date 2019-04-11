62ba90c70f9b0e40d9087d3cbe5025f7c46d7bce
fengshuaiju <shuaiju.feng@jjcargo.com>
2019-04-11 16:54:45 +0800

    INIT

--- README.md
+++
@@ -1,1 +0,0 @@
-JOOQ utils     https://simpleflatmapper.org/
--- src/main/java/com/feng/jooq/controller/JooqController.java
+++ src/main/java/com/feng/jooq/controller/JooqController.java
@@ -1,41 +1,25 @@
 package com.feng.jooq.controller;
 
-import com.feng.jooq.representation.Author;
-import com.feng.jooq.representation.BookDetails;
-import com.feng.jooq.service.JooqService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.feng.jooq.Persistence.AuthPersistence;
+import com.feng.jooq.entity.Author;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@Api(description = "用户&书籍接口")
-@RestController
-@RequiredArgsConstructor
+@RequestMapping("jooq")
 public class JooqController {
 
-    private final JooqService jooqService;
+    private final AuthPersistence authPersistence;
 
-    @ApiOperation(value = "查询所有作者" ,  notes="查询所有作者")
-    @GetMapping("/getAuthor")
-    public List<Author> getAuthors(){
-        return jooqService.getAuthors();
+    public JooqController(AuthPersistence authPersistence) {
+        this.authPersistence = authPersistence;
     }
 
-    @ApiOperation(value = "查询所有作者以及书籍信息" ,  notes="查询所有作者以及书籍信息")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "page", value = "页码", required = false ,dataType = "int"),
-            @ApiImplicitParam(name = "size", value = "页大小", required = false ,dataType = "int")
-    })
-    @GetMapping("/book-details")
-    public Page<BookDetails> bookDetails(Pageable pageable){
-        return jooqService.bookDetails(pageable);
+
+    @GetMapping
+    public List<Author> getAuthors(){
+        return authPersistence.findAllAuthor();
     }
 
 }
